b4554bac885b04ce8e3eccb79cf8ea3c4c5fe383
oz <857527916@qq.com>
2016-01-29 11:04:57 +0800

    commit

---
+++ Code/app/src/main/java/oz/code/Bean.java
@@ -0,0 +1,8 @@
+package oz.code;
+
+/**
+ * Created by Administrator on 2016/1/11.
+ */
+public class Bean
+{
+}
--- Code/transitiondemo/src/main/java/oz/code/administrator/transitiondemo/OttoActivity.java
+++ Code/transitiondemo/src/main/java/oz/code/administrator/transitiondemo/OttoActivity.java
@@ -1,7 +1,7 @@
 package oz.code.administrator.transitiondemo;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
 
